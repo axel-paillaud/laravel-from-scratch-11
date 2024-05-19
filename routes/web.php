072d5f9ed772553772386eb9fe7804b2dd559866
axel-paillaud <contact@axelweb.fr>
2024-05-19 12:05:33 +0200

    homework for video 2 ok

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/about', function () {
+    return 'This is about page';
+});
+
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
