c6d4ca224413da824e9e066a54389b765b5b1cb2
axel-paillaud <contact@axelweb.fr>
2024-05-23 06:30:52 +0200

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,7 @@
 <x-layout>
   <x-slot:heading>
     Home page
+    {{ $greetings }} !
   </x-slot:heading>
 
   <h2>Hello, from home page !</h2>
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,45 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        'greetings' => 'Hello',
+    ]);
 });
 
 
 Route::get('/about', function () {
     return view('about');
+});
+
+Route::get('/jobs/{id}', function($id) {
+
+    $jobs = [
+        [
+            'id' => 1,
+            'name' => 'Director',
+            'salary' => '50,000',
+        ],
+        [
+            'id' => 2,
+            'name' => 'Programmer',
+            'salary' => '100,000',
+        ],
+        [
+            'id' => 3,
+            'name' => 'Teacher',
+            'salary' => '40,000',
+        ],
+    ];
+
+    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+    dd($job);
+
+    return view('job', [
+        'job' => $job,
+    ]);
 });
 
 
