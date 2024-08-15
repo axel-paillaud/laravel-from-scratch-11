7c4ca05a2b435156bbd0c65745b4e6c5e7c31027
axel-paillaud <contact@axelweb.fr>
2024-08-15 11:54:06 +0200

    update

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -15,7 +15,14 @@
             <label for="title" class="block text-sm font-medium leading-6 text-gray-900">Job title</label>
             <div class="mt-2">
               <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                <input type="text" name="title" id="title" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Shift Leader">
+                <input
+                  type="text"
+                  name="title"
+                  id="title"
+                  class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                  placeholder="Shift Leader"
+                  required
+                >
               </div>
 
               @error('title')
@@ -30,7 +37,14 @@
             <label for="salary" class="block text-sm font-medium leading-6 text-gray-900">Job salary</label>
             <div class="mt-2">
               <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                <input type="text" name="salary" id="salary" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="$50,000 Per Year">
+                <input
+                  type="text"
+                  name="salary"
+                  id="salary"
+                  class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                  placeholder="$50,000 Per Year"
+                  required
+                >
               </div>
 
               @error('salary')
