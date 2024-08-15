ec953b36e8e6e77deeedfea17b705d19dbbb3e05
axel-paillaud <contact@axelweb.fr>
2024-08-15 11:44:48 +0200

    update

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -36,7 +36,7 @@
           @if($errors->any())
           <ul>
             @foreach($errors->all() as $error)
-              <li class="text-red-500">{{ $error }}</li>
+              <li class="text-red-500 italic">{{ $error }}</li>
             @endforeach
           </ul>
           @endif
