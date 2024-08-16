3c5c38e4f17e7ccf4228a630fad0c1b2dfb481d4
axel-paillaud <contact@axelweb.fr>
2024-08-16 10:48:32 +0200

    edit and destroy wip

--- routes/web.php
+++ routes/web.php
@@ -62,6 +62,41 @@ Route::get("/jobs/{id}/edit", function ($id) {
     ]);
 });
 
+// Update
+Route::patch("/jobs/{id}", function ($id) {
+    request()->validate([
+        "title" => ["required", "min:3"],
+        "salary" => ["required"],
+    ]);
+
+    // authorize (On hold ...)
+
+    // findOrFail, instead of find, to handle inexisting id
+    $job = Job::findOrFail($id);
+
+    // One way to do it
+    // $job->title = request("title");
+    // $job->salary = request("salary");
+    // $job->save();
+
+    // Another way to do it, similar to create method
+    $job->update([
+        "title" => request("title"),
+        "salary" => request("salary"),
+    ]);
+
+    return redirect("/jobs" . $job->id);
+});
+
+// Destroy
+Route::delete("/jobs/{id}", function ($id) {
+    // authorize (On hold ...)
+
+    Job::findOrFail($id)->delete();
+
+    return redirect("/jobs");
+});
+
 Route::get("/contact", function () {
     return view("contact");
 });
