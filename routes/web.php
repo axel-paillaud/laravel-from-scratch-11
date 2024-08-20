7dfbfad368b85ba9d2fd5a7bac51eb12fdc1b593
axel-paillaud <contact@axelweb.fr>
2024-08-20 15:55:36 +0200

    remove useless comment and import

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Models\Job;
 use App\Http\Controllers\JobController;
 use Illuminate\Http\Client\Request;
 
@@ -15,25 +14,12 @@ Route::get("/about", function () {
     return view("about");
 });
 
-// Index
 Route::get("jobs", [JobController::class, "index"]);
-
-// Create
 Route::get("jobs/create", [JobController::class, "create"]);
-
-// Show
 Route::get("/jobs/{job}", [JobController::class, "show"]);
-
-// Store
 Route::post("/jobs", [JobController::class, "store"]);
-
-// Edit
 Route::get("/jobs/{job}/edit", [JobController::class, "edit"]);
-
-// Update
 Route::patch("/jobs/{job}", [JobController::class, "update"]);
-
-// Destroy
 Route::delete("/jobs/{job}", [JobController::class, "destroy"]);
 
 Route::get("/contact", function () {
