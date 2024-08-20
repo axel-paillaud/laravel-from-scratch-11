fafab745670ba3ea350c0acece934a61f7cde2ea
axel-paillaud <contact@axelweb.fr>
2024-08-20 15:52:20 +0200

    use Route Controller

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -3,31 +3,91 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Job;
 
 class JobController extends Controller
 {
     public function index()
     {
-        dd('hello');
+        $jobs = Job::with("employer")->latest()->cursorPaginate(3);
+
+        return view("jobs.index", [
+            "jobs" => $jobs,
+        ]);
     }
 
     public function create()
     {
-
+        return view("jobs.create");
     }
 
-    public function show()
+    public function show(Job $job)
     {
-
+        return view("jobs.show", [
+            "job" => $job,
+        ]);
     }
 
     public function store()
     {
+        request()->validate([
+            "title" => ["required", "min:3"],
+            "salary" => ["required"],
+        ]);
 
+        Job::create([
+            "title" => request("title"),
+            "salary" => request("salary"),
+            "employer_id" => 1,
+        ]);
+
+        return redirect("/jobs");
     }
 
-    public function destroy()
+    public function edit(Job $job)
     {
+        // $job = Job::find($id);
+
+        return view("jobs.edit", [
+            "job" => $job,
+        ]);
+    }
+
+    public function update(Job $job)
+    {
+        // authorize (On hold ...)
+        //
+        request()->validate([
+            "title" => ["required", "min:3"],
+            "salary" => ["required"],
+        ]);
+
+        // findOrFail, instead of find, to handle inexisting id
+        // $job = Job::findOrFail($id);
+
+        // One way to do it
+        // $job->title = request("title");
+        // $job->salary = request("salary");
+        // $job->save();
+
+        // Another way to do it, similar to create method
+        $job->update([
+            "title" => request("title"),
+            "salary" => request("salary"),
+        ]);
+
+        return redirect("/jobs/" . $job->id);
 
     }
+
+    public function destroy(Job $job)
+    {
+        // authorize (On hold ...)
+
+        // Job::findOrFail($id)->delete();
+        $job->delete();
+
+        return redirect("/jobs");
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -16,91 +16,25 @@ Route::get("/about", function () {
 });
 
 // Index
-Route::get('jobs', [JobController::class, 'index']);
-/* Route::get("/jobs", function () {
-    $jobs = Job::with("employer")->latest()->cursorPaginate(3);
-
-    return view("jobs.index", [
-        "jobs" => $jobs,
-    ]);
-}); */
+Route::get("jobs", [JobController::class, "index"]);
 
 // Create
-Route::get("/jobs/create", function () {
-    return view("jobs.create");
-});
+Route::get("jobs/create", [JobController::class, "create"]);
 
 // Show
-Route::get("/jobs/{job}", function (Job $job) {
-    // Without Route Model Binding :
-    // $job = Job::find($id);
-
-    return view("jobs.show", [
-        "job" => $job,
-    ]);
-});
+Route::get("/jobs/{job}", [JobController::class, "show"]);
 
 // Store
-Route::post("/jobs", function () {
-    request()->validate([
-        "title" => ["required", "min:3"],
-        "salary" => ["required"],
-    ]);
-
-    Job::create([
-        "title" => request("title"),
-        "salary" => request("salary"),
-        "employer_id" => 1,
-    ]);
-
-    return redirect("/jobs");
-});
+Route::post("/jobs", [JobController::class, "store"]);
 
 // Edit
-Route::get("/jobs/{job}/edit", function (Job $job) {
-    // $job = Job::find($id);
-
-    return view("jobs.edit", [
-        "job" => $job,
-    ]);
-});
+Route::get("/jobs/{job}/edit", [JobController::class, "edit"]);
 
 // Update
-Route::patch("/jobs/{job}", function (Job $job) {
-    // authorize (On hold ...)
-    //
-    request()->validate([
-        "title" => ["required", "min:3"],
-        "salary" => ["required"],
-    ]);
-
-
-    // findOrFail, instead of find, to handle inexisting id
-    // $job = Job::findOrFail($id);
-
-    // One way to do it
-    // $job->title = request("title");
-    // $job->salary = request("salary");
-    // $job->save();
-
-    // Another way to do it, similar to create method
-    $job->update([
-        "title" => request("title"),
-        "salary" => request("salary"),
-    ]);
-
-    return redirect("/jobs/" . $job->id);
-});
+Route::patch("/jobs/{job}", [JobController::class, "update"]);
 
 // Destroy
-Route::delete("/jobs/{job}", function (Job $job) {
-    // authorize (On hold ...)
-
-    // Job::findOrFail($id)->delete();
-    $job->delete();
-
-    return redirect("/jobs");
-});
+Route::delete("/jobs/{job}", [JobController::class, "destroy"]);
 
 Route::get("/contact", function () {
     return view("contact");
